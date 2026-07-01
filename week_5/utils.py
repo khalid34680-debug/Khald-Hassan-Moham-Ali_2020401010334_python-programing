122970d48922e14edc41bc977229e6328b1e88fe
khalid34680-debug <khalid34680@gmail.com>
2026-07-01 12:10:52 +0800

    Update utils.py

--- week_5/utils.py
+++ week_5/utils.py
@@ -1,1 +1,13 @@
+def calculate_total(coffee, tea, sandwich):
+    total = coffee * 8.50 + tea * 6.00 + sandwich * 12.00
+    return total
 
+
+def print_receipt(customer_name, coffee, tea, sandwich, total):
+    print("===== RECEIPT =====")
+    print("Customer :", customer_name)
+    print("Coffee   :", coffee)
+    print("Tea      :", tea)
+    print("Sandwich :", sandwich)
+    print("-------------------")
+    print(f"Total = RM {total:.2f}")
