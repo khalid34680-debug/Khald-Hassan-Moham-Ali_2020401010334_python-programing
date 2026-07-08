dd0dcbf3c2596a52c2ca518872067805bbcc1320
khalid34680-debug <khalid34680@gmail.com>
2026-07-08 12:00:00 +0800

    Create test_food_order.py

---
+++ week_7/test_food_order.py
@@ -0,0 +1,16 @@
+from food_order import calculate_total
+
+def test_order1():
+    assert calculate_total(10, 2) == 20
+
+def test_order2():
+    assert calculate_total(10, 3) == 30
+
+def test_order3():
+    assert calculate_total(10, 10) == 100
+
+def test_invalid_price():
+    assert calculate_total(-10, 2) == "invalid price"
+
+def test_invalid_quantity():
+    assert calculate_total(10, 0) == "invalid quantity"
