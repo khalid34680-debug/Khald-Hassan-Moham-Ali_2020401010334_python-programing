084a39839243d9a02d3e9fb0bdab63903405d176
khalid34680-debug <khalid34680@gmail.com>
2026-08-12 13:28:04 +0800

    Create lab_monitor.py

---
+++ week_12/lab_monitor.py
@@ -0,0 +1,36 @@
+def check_computers():
+    computers = []  
+
+    for number in range(5):
+
+       e
+        status = input(
+            f"Computer {number + 1} Status (A/U/M): "
+        ).upper()
+
+        computers.append(status)
+
+    return computers
+
+
+def count_available(computers):
+    available = 0 
+
+    for status in computers:
+        if status == "A":
+            available += 1
+
+    return available
+
+
+def display_status(computers, available):
+    print("\n========== LAB STATUS ==========")
+
+    for number in range(5):
+        print(
+            f"Computer {number + 1}: {computers[number]}"
+        )
+
+    print("------------------------------")
+    print(f"Available Computers: {available}")
+    print("================================")
